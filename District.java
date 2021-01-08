9df3dae36a4606d2cd218de327a65508adcc56f0
45738070 <hardyjules@icloud.com>
2021-01-08 13:55:04 +0100

    Update District.java

--- District.java
+++ District.java
@@ -4,15 +4,13 @@ public class District extends Tile {
     private boolean north, east, south, west;
 
 
-    public District(Alibi alibi, List<Boolean> orientation){
+    public District(Alibi alibi, List<Boolean> orientation) {
         this.alibi = alibi;
         this.north = orientation.get(0);
         this.east = orientation.get(1);
         this.south = orientation.get(2);
         this.west = orientation.get(3);
     }
-
-
 
     public Alibi getAlibi(){
         return alibi;
